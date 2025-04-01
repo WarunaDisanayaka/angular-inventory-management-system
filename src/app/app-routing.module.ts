f10f657114a97e385f4871f5d5f318d68aa49715
Waruna <86902580+WarunaDisanayaka@users.noreply.github.com>
2025-04-01 23:01:38 +0530

    Updated with major features

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { RegistrationListComponent } from './registration-list/registration-list.component';
 import { RegistrationFormComponent } from './registration-form/registration-form.component';
+import { PrivacySecurityComponent } from './privacy-security/privacy-security.component';
+import { HelpPageComponent } from './help-page/help-page.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: HomepageComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'list', component: RegistrationListComponent },
   { path: 'new', component: RegistrationFormComponent },
   { path: 'edit/:id', component: RegistrationFormComponent },
+  { path: 'privacy-security', component: PrivacySecurityComponent },
+  { path: 'help', component: HelpPageComponent },
+
+
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { PrivacySecurityComponent } from './privacy-security/privacy-security.component';
+import { HelpPageComponent } from './help-page/help-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    PrivacySecurityComponent,
+    HelpPageComponent
   ],
   imports: [
     BrowserModule,
